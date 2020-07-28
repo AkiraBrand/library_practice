923f94744ef14b1f259de0e0f4901d97d9bbd46a
Akira <akira.brand@yahoo.com>
2020-07-28 19:38:18 +0000

    cleans up code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//variables 
 var title = "The Bible";
 var pages = 4000;
 var pages = (pages/4);
@@ -6,6 +7,7 @@ var sequel = `${title} 2: the sequel`;
 var isFavorite = true; 
 var isShortStory = pages < 25;
 
+//conditionals
 if (pages < 1000) {
   console.log("I could read that in a day!");
 } else {
@@ -18,11 +20,14 @@ if (isFavorite === true) {
   console.log(`I recently read ${title}.`);
 };
 
+//arrays
 var books = ["The Bible", "On Writing", "The Shining", "A Wild Sheep Chase", "The Wisdom of Uncertainty"];
 var thirdBook = books[2];
-
 
 var belowTwoHundred = [23, 76, 167, 0.1, -5, 1, 32, 12, 87.8, 10, 13, 98];
 var fifthElement = belowTwoHundred[4];
 console.log(fifthElement);
+
+//
+
 
