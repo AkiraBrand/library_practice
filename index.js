e0c8b9768781e6269192c5352877b1258f13ab42
Akira <akira.brand@yahoo.com>
2020-07-28 19:32:43 +0000

    adds conditionals

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ var movie = undefined;
 var sequel = `${title} 2: the sequel`;
 var isFavorite = true; 
 var isShortStory = pages < 25;
+
+if (pages < 1000) {
+  console.log("I could read that in a day!");
+} else {
+  console.log("Ok, maybe I could read that in a day and a half");
+};
+
+if (isFavorite === true) {
+  console.log(`My favorite book is ${title}.`);
+} else {
+  console.log(`I recently read ${title}.`);
+};
