a52e05fe190040257032f30b3fcc848712dfe531
Akira <akira.brand@yahoo.com>
2020-07-28 19:18:08 +0000

    all variables added

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 var title = "The Bible";
 var pages = 4000;
-console.log(pages);
-
 var pages = (pages/4);
-
-console.log(pages);
-
 var movie = undefined;
-
-console.log(movie);
-
 var sequel = `${title} 2: the sequel`;
-
-console.log(sequel);
+var isFavorite = true; 
+var isShortStory = pages < 25;
